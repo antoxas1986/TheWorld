d76a5ec6d786f63f16f077f3d93f9fd461ed8865
Anton Kuznetsov <kuznetsov74@gmail.com>
2015-12-07 22:36:22 -0500

    my commit thru sourceTree

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,12 +2,19 @@
 
     var $menu = $("#menuButton");
     var sidebarandWrapper = $("#sidebar, #wrapper");
+    var icon = $("#menuButton i.fa");
 
     $menu.on("click", function () {
         sidebarandWrapper.toggleClass("hide-sidebar");
+        if (sidebarandWrapper.hasClass("hide-sidebar")) {
+            icon.removeClass("fa-angle-left");
+            icon.addClass("fa-angle-right");
+        } else {
+            icon.addClass("fa-angle-left");
+            icon.removeClass("fa-angle-right");
+        }
     });
-    var $username = $("#username");
-    $username.text("Antoxas");
+   
 
     
 
